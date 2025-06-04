53e4cf772a0c6557efed6cedcace12dcc7446a4f
ossanaTomas <ossanatomas5@gmail.com>
2025-06-03 22:54:41 -0300

    agregado de manejo de videos

--- Frontend/mi-app/src/Components/Firedetector.jsx
+++ Frontend/mi-app/src/Components/Firedetector.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 export default function FireDetector() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [resultImage, setResultImage] = useState(null);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [resultvideo, setResultVideo] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,6 +32,35 @@ export default function FireDetector() {
     }
   };
 
+
+  const handleSubmit2 = async (e) => {
+    e.preventDefault();
+    if (!selectedVideo) return alert("Elegí un video");
+
+    const formData = new FormData();
+    formData.append("file", selectedVideo);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8001/predict/video/", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.detail || "Error al detectar fuego");
+      }
+
+      const blob = await response.blob();
+      setResultVideo(URL.createObjectURL(blob));
+    } catch (err) {
+      alert("Error: " + err.message);
+    }
+  };
+
+
+
+
   return (
     <div>
       <h1>Subí una imagen para detectar fuego 🔥</h1>
@@ -42,6 +73,24 @@ export default function FireDetector() {
         />
         <button type="submit">Detectar</button>
       </form>
+      <h1>Subí un video para detectar fuego fuego 🔥</h1>
+      <form onSubmit={handleSubmit2}>
+        <input
+          type="file"
+          accept="video/*"
+          onChange={(e) => setSelectedVideo(e.target.files[0])}
+          required
+        />
+        <button type="submit">Detect</button>
+      </form>
+
+      {resultvideo && (
+        <div>
+          <h2>Resultado del video:</h2>
+          <video src={resultvideo} controls style={{ maxWidth: "500px" }} />
+        </div>
+      )}
+
       {resultImage && (
         <div>
           <h2>Resultado:</h2>
