22b76dc3b1e5616ed004528af95a07b7848c2de3
ossanaTomas <ossanatomas5@gmail.com>
2025-06-10 23:40:14 -0300

    Mejora manejo video

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -57,18 +57,15 @@ def predict_image(file: UploadFile = File(...),confidence:float = Form(...)):
 
 
 @app.post("/predict/video/")
-async def predict_video(file: UploadFile = File(...)):
-    try:
-        # Leer el video en memoria
+async def predict_video(file: UploadFile = File(...), confidence:float = Form(...), cant_frames:int=Form(...)):
+    try: 
         print("comenzando a guardar video")
-        incio=time.time()
+        #creo archivo temporal de tipo .mp4, delete false significa que no se borre al terminar el bloque
         with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
             tmp.write(await file.read())
             tmp_path = tmp.name
-        fin=time.time()
-        print("fin guardado",fin-incio)
         print(f"Tamaño del video cargado: {os.path.getsize(tmp_path)} bytes")
-     
+        
         cap = cv2.VideoCapture(tmp_path)
         print("Video abierto")
         if not cap.isOpened():
@@ -78,28 +75,30 @@ async def predict_video(file: UploadFile = File(...)):
         fps = cap.get(cv2.CAP_PROP_FPS)
         ancho = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         alto = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp_output:
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".avi") as tmp_output: #creo otro arc tmp pero para la salida por eso .avi
             output_path = tmp_output.name
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec mp4v
-        out = cv2.VideoWriter(output_path, fourcc, fps, (ancho, alto))
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Codec mp4v
+        out = cv2.VideoWriter(output_path, fourcc, fps, (ancho//2, alto//2))
 
         frame_index=0
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
-            print(f"Procesando frame {frame_index}")
-            if frame_index % 4 == 0:
-                # Detectar con tu modelo YOLO
+            
+            #cada que cantidad de frames se realiza un analisis
+            if frame_index % cant_frames == 0:
+                
                 start=time.time()
-                resultados = fire_detector.predict_video(frame, imgsz=416, conf=0.3)
+                resultados = fire_detector.predict_video(frame, imgsz=416, conf=confidence)
                 result = resultados[0]
-                print("Tiempo por frame:", time.time() - start)# Dibujar resultados en el frame
+                print("Procesando frame:",frame_index," Tiempo por frame:", time.time() - start)# Dibujar resultados en el frame
                 
             annotated_frame=fire_detector.dibujar_anotaciones(frame, result)
-            
+            #redimenciono antes de escribir
+            annotated_frame = cv2.resize(annotated_frame, (ancho // 2, alto // 2))
             out.write(annotated_frame)
             frame_index += 1
 
@@ -108,10 +107,10 @@ async def predict_video(file: UploadFile = File(...)):
         print(f"Video procesado intermedio guardado en {output_path}, tamaño: {os.path.getsize(output_path)} bytes")
 
         #Conversión final a H.264
-        output_h264 = output_path.replace(".mp4", "_h264.mp4")
+        output_h264 = output_path.replace(".avi", "_h264.mp4")
         subprocess.run([
              ffmpeg_path, "-y", "-i", output_path,
-             "-c:v", "libx264", "-preset", "fast", "-crf", "23",
+               "-c:v", "libx264", "-preset", "ultrafast", "-crf", "32",
               output_h264
         ], check=True)
 
@@ -123,11 +122,14 @@ async def predict_video(file: UploadFile = File(...)):
         import traceback
         traceback.print_exc()
         return {"error": str(e)}
+    
+    
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+
     while True:
         data = await websocket.receive_bytes()
         np_data = np.frombuffer(data, np.uint8)
@@ -138,6 +140,8 @@ async def websocket_endpoint(websocket: WebSocket):
         # Codificar imagen de nuevo para enviar
         _, buffer = cv2.imencode('.jpg', detections)
         await websocket.send_bytes(buffer.tobytes())
+
+
 
 @app.post("/predict/video/mjpeg/")
 def predict_video_mjpeg(file: UploadFile = File(...)):
@@ -183,7 +187,7 @@ def predict_video_mjpeg(file: UploadFile = File(...)):
 @app.post("/stream_video/")
 async def stream_video(file: UploadFile = File(...)):
     try:
-        # Leer el video en memoria
+        
         contents = await file.read()
         input_buffer = io.BytesIO(contents)
         container = av.open(input_buffer)
